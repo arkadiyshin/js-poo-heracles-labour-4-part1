1263fe1f220cf9cdd549a182c896aeafd4fa20f9
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-08-17 16:19:53 +0200

    heracles labour 4 part 1

--- src/Arena.js
+++ src/Arena.js
@@ -3,6 +3,7 @@ class Arena {
     this.hero = hero
     this.monsters = monsters
     this.size = size
+    this.message = JSON.stringify( { x: hero.x, y: hero.y} );
   }
 
   getDistance(fighter1, fighter2) {
@@ -16,8 +17,52 @@ class Arena {
   // Hero movement managment
   // Gestion du déplacement du héros
   move(direction) {
-    /* Your code goes here */ 
-  
+    /* Your code goes here */
+    const oldPosition = {x: this.hero.x, y: this.hero.y}
+    const newPosition = {x: this.hero.x, y: this.hero.y}
+
+    switch (direction) {
+      case "N":
+        newPosition.y-=1;
+      break;
+      case "S":
+        newPosition.y+=1;
+      break;
+      case "W":
+        newPosition.x+=1;
+      break;
+      case "E":
+        newPosition.x-=1;
+      break;
+    } 
+    
+    if(this.isMapExit(newPosition)) {
+      this.message = 'The map exit';
+    } else if(this.isOccupied(newPosition)) {
+      this.message = 'The square is occupied';
+    } else {
+      this.hero.x = newPosition.x;
+      this.hero.y = newPosition.y;
+      this.message = JSON.stringify(newPosition);
+    }
+    this.showMessage();
+
     return oldPosition;
+  }
+
+  isMapExit(position) {
+    return (position.x < 0 || position.x >= this.size || position.y < 0 || position.y >= this.size);
+  }
+
+  isOccupied(position) {
+    for(let monster of this.monsters) {
+      if(position.x === monster.x && position.y === monster.y) return true;
+    }
+    return false;
+  }
+
+  showMessage() {
+    const errorBlock = document.getElementById('error');
+    errorBlock.innerText = this.message;
   }
 }
